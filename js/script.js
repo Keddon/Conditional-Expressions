5851056209908cca841e6d17d2b2ece78e0a31e6
Keddon Campbell <mrkfed101@gmail.com>
2015-02-19 15:20:18 -0500

    Conditional Ecpressions
    Understanding the difference between the assignment and the equality
    functions. The introduction to If statements.

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@
  */
 
 
+var oldEnough = true;
+//if the child is old enough, print to the console "you are old enough."
+
+if(oldEnough === true){  // there is a difference between =, == and ===
+    console.log("you are old enough to ride!"); //code performed if condition is true
+}
