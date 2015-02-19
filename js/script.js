0ff20fbe0a626d9d42079494dc216bf3422e0002
Keddon Campbell <mrkfed101@gmail.com>
2015-02-19 15:35:24 -0500

    Relational Expressions
    using if, else if and else.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,16 @@
 
 var kidHeight = 30;
 var minHeight = 48;
+var sneakerLift = 3;
+var wParentHeight = 45;
 //if the child is over 48 inches in height.
 //var oldEnough = true;
 //if the child is old enough, print to the console "you are old enough."
 
-if(kidHeight > minHeight){  // there is a difference between =, == and ===
+if(kidHeight + sneakerLift > minHeight){  // there is a difference between =, == and ===
     console.log("you are old enough to ride!"); //code performed if condition is true
+}else if (kidHeight > wParentHeight){
+    console.log("you can but with a parent");
+}else{
+    console.log("Sorry you have some growing to do");
 }
